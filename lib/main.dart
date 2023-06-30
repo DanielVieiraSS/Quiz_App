392d6dd42962f445f0cf0b22a62822ad9fdc48d0
DanielVieiraSS <danielvieira2828@gmail.com>
2023-06-30 12:06:02 -0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/screens/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Colors.deepPurple,
+                Color.fromARGB(255, 86, 48, 153),
+              ],
+            ),
+          ),
+          child: const Home(),
+        ),
+      ),
+    );
+  }
+}
